1c5c5e2040689ad2316fd0374324e9d59570c3da
habibur-01 <h.r.habibur08@gmail.com>
2023-12-26 05:25:52 +0600

    post task to server

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const cors = require('cors')
-const port = 3000
+// const axios = require('axios');
+const port = process.env.PORT || 4000
 
 app.use(cors())
 app.use(express.json())
+
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cbqlcas.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,9 +24,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    const userCollection =  client.db('TaskManager').collection('user')
-    const taskCollection = client.db('TaskManager').collection('task')
+    await client.connect();
+
+    const userCollection =  client.db('taskManagement').collection('user')
+    // const taskCollection = client.db('TaskManager').collection('task')
+    const taskCollection = client.db("taskManagement").collection("task")
+
 
     app.post('/user', async(req, res) => {
       const userInfo = req.body
@@ -32,6 +38,18 @@ async function run() {
     })
     app.get('/user', async(req, res) => {
       const cursor = userCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post("/task", async(req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task)
+      res.send(result)
+    })
+
+    app.get('/task', async(req, res) => {
+      const cursor = taskCollection.find()
       const result = await cursor.toArray()
       res.send(result)
     })
